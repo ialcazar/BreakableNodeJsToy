051f76332d3604ce8cebed8ff69501084e022376
Israel Alcázar <israelalcazar@gmail.com>
2012-08-09 08:12:14 +0200

    Added Authorization header to resolve Cross Domain problems

--- web/filters.js
+++ web/filters.js
@@ -11,7 +11,7 @@ var filters = module.exports = function(app, logger){
     res.removeHeader("X-Powered-By");
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-Requested-With, Content-Type');
     next();
   });
 
